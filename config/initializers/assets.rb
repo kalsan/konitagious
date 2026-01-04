b5c3804eba896d5c1eefa7276a4b9dc29c74e85c
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2026-01-04 14:23:12 +0100

    Install and setup bootstrap and dartsass

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# https://github.com/twbs/bootstrap-rubygem README states to add:
+Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js]
